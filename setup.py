d4a5e607186f2f429e4b68b54c6eaf8dd5123919
Renan Palmeira <renan@forleven.com>
2016-01-01 02:59:20 -0200

    Uploaded last changes in PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='neodjango',
-    version='0.0.3',
+    version='0.0.4',
     author='Renan Palmeira',
     author_email='renanpalmeira1@hotmail.com',
     description='A Django/Neo4j ORM layer.',
