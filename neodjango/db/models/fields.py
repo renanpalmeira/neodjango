06d6779e248c8b95b6a0fd00afddb00ab55f2a96
Renan Palmeira <renan@forleven.com>
2016-01-06 23:19:12 -0200

    Adding new signals and initial fields for OGM (Object Graph Mapper). http://goo.gl/MBiMFC

---
+++ neodjango/db/models/fields.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+class Field(object):
+	pass
+
+class StringProperty(Field):
+	pass
--- neodjango/db/models/signals.py
+++ neodjango/db/models/signals.py
@@ -3,4 +3,15 @@ from __future__ import absolute_import
 from django.dispatch import Signal
 
 node_prepared = Signal()
+
+"""
+this signals get when __init__ started and finished
+"""
 pre_node_init = Signal(providing_args=['instance', ])
+post_node_init = Signal(providing_args=['instance', ])
+
+"""
+this signals get when Node save started and finished
+"""
+pre_node_save = Signal(providing_args=['instance', ])
+post_node_save = Signal(providing_args=['instance', ])
